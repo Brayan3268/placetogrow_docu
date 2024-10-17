10555e562a4837ee25349ce0a0c47a1c797c6747
Brayan <brayanlujan.mzu@hotmail.com>
2024-10-16 21:59:12 -0500

    Test deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://placetogrow_docu',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/placetogrow_docu/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
